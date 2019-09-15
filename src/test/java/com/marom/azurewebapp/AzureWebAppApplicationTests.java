a878813a7335c51ecd68b0db129eda5b978ba9ba
marom <maro.muszynski@gmail.com>
2019-09-15 11:15:24 +0100

    initial commit

---
+++ src/test/java/com/marom/azurewebapp/AzureWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marom.azurewebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AzureWebAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
